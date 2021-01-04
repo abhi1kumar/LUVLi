1364e58c339c733931ac8bdbf2a1da744223aa49
abhi1kumar <abhinav3663@gmail.com>
2021-01-04 21:58:39 +0530

    Pointed to MERL repo

--- test/get_MLP_size.py
+++
@@ -1,9 +0,0 @@
-import torch
-from torchsummary import summary
-import torch.nn as nn
-
-model = nn.Sequential(nn.Linear(2048, 4096), nn.ReLU(), nn.Linear(4096, 4096), nn.ReLU(), nn.Linear(4096, 204))
-print(model)
-model = model.cuda()
-
-summary(model,(1,2048))
